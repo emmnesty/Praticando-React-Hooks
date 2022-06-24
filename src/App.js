a41680451c3b34ad88f9b006ec5fafda06822cad
emmnesty <elyjhonatan@hotmail.com>
2022-06-24 13:09:20 -0300

    Praticando com Hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import styles from './avatar.module.css';
 
-function App() {
+export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        'https://api.github.com/users/emmnesty/repos'
+      );
+      const data = await response.json();
+      console.log(data);
+      setRepositories(data);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul className={styles.ulPai}>
+      {repositories.map((repo) => (
+        <div className={styles.corDoFundo} key={repo.id}>
+          {repo.name}
+          <img
+            src={repo.owner.avatar_url}
+            alt=''
+            className={styles.imagemIcon}
+          />
+        </div>
+      ))}
+    </ul>
   );
 }
-
-export default App;
